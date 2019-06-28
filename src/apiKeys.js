8b354e7e540b557b5c43f51ac597e39f8a02843a
asantoss <alexsantosantana@live.com>
2019-06-28 14:28:38 -0400

    Revert "Delete apiKeys.js"
    This reverts commit 918c0f148f509047d29d3341c3be4063e281ebb1.

---
+++ src/apiKeys.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDMHRUmIUG-azrlNMjpY4VsUMEkgF5Ixx8",
+    authDomain: "entertainme-e1073.firebaseapp.com",
+    databaseURL: "https://entertainme-e1073.firebaseio.com",
+    projectId: "entertainme-e1073",
+    storageBucket: "",
+    messagingSenderId: "801025173413",
+    appId: "1:801025173413:web:5767a5fcdc7a5d90"
+};
