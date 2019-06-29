605826845c60ee1483e7ff9d63f3a079158a7752
Alexander Santos <alexsantosantana@live.com>
2019-06-29 10:34:18 -0400

    Youtube functionality is set

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 var apiResults = $('#apiResults')
 $('#entertainMeBtn').click(() => {
-
     //This portion sets our iframe element to hide
-    $('#player').css('opacity', '0');
-    $('#player').css('display', 'none');
-    $('#player').attr('src', ``)
-    let random = Math.floor((Math.random() * 4) + 1);
+    youtubeVideoDiv.css('opacity', '0');
+    youtubeVideoDiv.css('display', 'none');
+    youtubeVideoDiv.attr('src', ``);
+    let random = Math.floor((Math.random() * 3) + 1);
     console.log(random)
     if (random === 1) {
         giveMeRandomVid()
+    } else if (random === 2) {
+        getNYT()
+    } else if (random === 3) {
+        getNYTMoviesPick()
     }
 })
--- src/youtubeApi.js
+++ src/youtubeApi.js
@@ -1,4 +1,4 @@
-var targetDiv = $('#player');
+var youtubeVideoDiv = $('#player');
 //This array contains a set of video id's from Tasty
 var bestCookingVidsId = [`AL6GWJdzhUg`, 'z4L2E6_Gmkk', 'ir5U88d_3iI', 'gauyUvRIPzs']
 
@@ -10,19 +10,20 @@ var giveMeRandomVid = () => {
         .then(results => {
             var random = randNum(1, results.pageInfo.resultsPerPage)
             var video = results.items[random].id.videoId
-            targetDiv.attr('src', `https://www.youtube.com/embed/${video}?enablejsapi=1&amp;origin=http%3A%2F%2F127.0.0.1%3A5500&amp;widgetid=1`)
-            targetDiv.css('display', 'block');
+            youtubeVideoDiv.attr('src', `https://www.youtube.com/embed/${video}?enablejsapi=1&amp;origin=http%3A%2F%2F127.0.0.1%3A5500&amp;widgetid=1`)
+            youtubeVideoDiv.css('display', 'block');
             $('#entertainMeBtn').removeClass('disabled');
-            targetDiv.css('opacity', '1');
-            targetDiv.css('animation', 'videoOn 1s ease-in')
+            youtubeVideoDiv.css('opacity', '1');
+            youtubeVideoDiv.css('animation', 'videoOn 1s ease-in')
         })
 
     //After our ajax request is fufilled we set the iframe to display block and add a src attribute
 }
 
 
+
 //This function just gives me a random number from a min & max
-var randNum = function(min, max) {
+var randNum = function (min, max) {
     if (Number.isInteger(min) && Number.isInteger(max)) {
         return Math.floor((Math.random() * max) + min);
     } else {
