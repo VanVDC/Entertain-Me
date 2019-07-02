55f4b623550bb55f6803c04baa888de11093fe6e
VanVDC <vanbvy@gmail.com>
2019-07-02 18:40:52 -0400

    Finish with my movie(themoviedb) and book(NYT) APIs.

--- src/vanApi.js
+++ src/vanApi.js
@@ -1,102 +1,70 @@
-// //movies api
 
 
-async function getMovies(t){
-    const urlLink ='http://www.omdbapi.com/?apikey=3430a78&s=';
-    const movieSearch=urlLink+t;
-    const rawMovies =await fetch(movieSearch);
-    const movie = await rawMovies.json();
-    const movData=movie.Search; 
+async function getNYT(){
+       
+        const nytURL='https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+key.NYT;
+        const rawNYTBooks= await fetch(nytURL);
+        const nytbooks=await rawNYTBooks.json(); //create a New York Time URL
+        let ran=Math.floor((Math.random() * 15) + 1) // create a random number for API index
+        //data
+        let {title, author, book_image, description, amazon_product_url}=nytbooks.results.books[ran];
 
-    for(var i=0; i<movData.length;i++){
-        const {Title,Poster,Year}=movie.Search[i]; 
-        if(Title==t&&Year>=2018&&Poster!="N/A"){
-            return Poster;
+        console.log(book_image);
+        console.log(title);
+        console.log(author);
+        console.log(description);
+        console.log(amazon_product_url);
 
-        }
+    
+        let img = document.getElementById('img'); // get the img tag
+        img.setAttribute('src', book_image);
+        document.getElementById('title').textContent=title; //render the title
+        
+        document.getElementById('img').appendChild(img);
+        document.getElementById('summary').textContent=description;//render the summary
+    
+        let a =document.createElement('a');
+        a.setAttribute('href', amazon_product_url); //render the amazon link
+        document.getElementById('amazon').appendChild(a);
+    
     }
+    
+    // getNYT();
+    
+
+const key1='?api_key='+key.tmdb;
+const baseUrl='https://api.themoviedb.org/3/movie/';
+const search={ //object for searches
+    latest:'latest',
+    popular:'popular',
+    now_playing:'now_playing',
+    top_rating:'top_rated'
+};
+const endUrl='ac&language=en-US&page=1';
+
+const imgLink='https://image.tmdb.org/t/p/w500';
+
+const url=baseUrl+search.popular+key1; //create the URL
+
+async function getMovies(){
+    let ran=Math.floor((Math.random() * 19) + 1) //crate a random number for the index
+
+    const rawData=await fetch(url); // get the raw data
+    const movieData= await rawData.json(); // get the movies data
+
+    const {poster_path,overview, original_title, }=movieData.results[ran]; //data
+
+    console.log(imgLink+poster_path);
+    console.log(original_title);
+    console.log(overview);
+
+    let img = document.getElementById('img'); // get the img tag
+    img.setAttribute('src', imgLink+poster_path); 
+    document.getElementById('title').textContent=original_title; //render the title
+    
+    document.getElementById('img').appendChild(img); //render the img
+    document.getElementById('summary').textContent=overview; // render the summary
+
 }
-
-
-
-//====================
-
-// //books api
-
-// const bookSearch='harrypotter';
-// const googleApi='https://www.googleapis.com/books/v1/volumes?q='+ bookSearch;
-
-// async function getBooks(){
-//     const rawBooks=await fetch(googleApi);
-//     const book = await rawBooks.json();
-//     const {title, authors, imageLinks }=book.items[0].volumeInfo;
-
-//     let img = document.createElement('img');
-//     img.setAttribute('src', imageLinks.thumbnail);
-    
-//     document.getElementById('render').appendChild(img);
-// }
-
-// getBooks();
-
-//=============================
-
-// NYT top books and movies
-
-
-var ranNum= Math.floor((Math.random() * 10) + 1);
-
-
-// async function getNYT(){
-//     const nytURL='https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+key.NYT;
-//     const rawNYTBooks= await fetch(nytURL);
-//     const nytbooks=await rawNYTBooks.json();
-//     //data
-//     let {title, author, book_image, description, amazon_product_url}=nytbooks.results.books[ranNum];
-
-//     let img = document.createElement('img');
-//     img.setAttribute('src', book_image);
-//     document.getElementById('title').textContent=title;
-    
-//     document.getElementById('apiResults').appendChild(img);
-//     document.getElementById('summary').textContent=description;
-
-//     let a =document.createElement('a');
-//     a.setAttribute('href', amazon_product_url);
-//     document.getElementById('amazon').appendChild(a);
-
-// }
-
-// getNYT();
-// //========================
-
-
-async function getNYTMoviesPick(){
-
-
-    const nytMovie='https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key='+key.NYT;
-    const rawNYTMovies= await fetch(nytMovie);
-    const nytMovies=await rawNYTMovies.json();
-    //data
-    const{display_title, summary_short, multimedia}=nytMovies.results[ranNum];
-
-
-    var pic =getMovies(display_title);
-
-    let img = document.createElement('img');
-
-    if(display_title=='')
-
-    img.setAttribute('src',multimedia.src);
-    
-
-    document.getElementById('title').textContent=display_title;
-    document.getElementById('apiResults').appendChild(img);
-    document.getElementById('title').textContent=summary_short;
-
-
-    
-}
-
-getNYTMoviesPick();
+// getMovies()
 
