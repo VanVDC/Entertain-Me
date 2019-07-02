7355a496ccd3c0e9ff3916ec78cf56987c562ac7
Alexander Santos <alexsantosantana@live.com>
2019-07-02 19:39:41 -0400

    we have all three apis functional

--- src/main.js
+++ src/main.js
@@ -3,15 +3,17 @@ $('#entertainMeBtn').click(() => {
     //This portion sets our iframe element to hide
     youtubeVideoDivContainer.css('opacity', '0');
     youtubeVideoDivContainer.css('display', 'none');
+    document.getElementById('nyt_tmdb').setAttribute('style', 'display: none;');
     stopVideo();
     let random = Math.floor((Math.random() * 3) + 1);
     console.log(random);
-    // if (random === 1) {
-    //     giveMeRandomVid()
-    // } else if (random === 2) {
-    //     getNYT()
-    // } else if (random === 3) {
-    //     getNYTMoviesPick()
-    // }
-    getNYTMoviesPick()
+    if (random === 1) {
+        giveMeRandomVid()
+    } else if (random === 2) {
+        document.getElementById('nyt_tmdb').setAttribute('style', 'display: block');
+        getNYT()
+    } else if (random === 3) {
+        document.getElementById('nyt_tmdb').setAttribute('style', 'display: block');
+        getMovies()
+    }
 })
--- src/vanApi.js
+++ src/vanApi.js
@@ -1,69 +1,65 @@
 
 
-async function getNYT(){
-       
-        const nytURL='https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+key.NYT;
-        const rawNYTBooks= await fetch(nytURL);
-        const nytbooks=await rawNYTBooks.json(); //create a New York Time URL
-        let ran=Math.floor((Math.random() * 15) + 1) // create a random number for API index
-        //data
-        let {title, author, book_image, description, amazon_product_url}=nytbooks.results.books[ran];
+async function getNYT() {
 
-        console.log(book_image);
-        console.log(title);
-        console.log(author);
-        console.log(description);
-        console.log(amazon_product_url);
+    const nytURL = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=' + key.NYT;
+    const rawNYTBooks = await fetch(nytURL);
+    const nytbooks = await rawNYTBooks.json(); //create a New York Time URL
+    let ran = Math.floor((Math.random() * 15) + 1) // create a random number for API index
+    //data
+    let { title, author, book_image, description, amazon_product_url } = nytbooks.results.books[ran];
 
-    
-        let img = document.getElementById('img'); // get the img tag
-        img.setAttribute('src', book_image);
-        document.getElementById('title').textContent=title; //render the title
-        
-        document.getElementById('img').appendChild(img);
-        document.getElementById('summary').textContent=description;//render the summary
-    
-        let a =document.createElement('a');
-        a.setAttribute('href', amazon_product_url); //render the amazon link
-        document.getElementById('amazon').appendChild(a);
-    
-    }
-    
-    // getNYT();
-    
+    console.log(book_image);
+    console.log(title);
+    console.log(author);
+    console.log(description);
+    console.log(amazon_product_url);
 
-const key1='?api_key='+key.tmdb;
-const baseUrl='https://api.themoviedb.org/3/movie/';
-const search={ //object for searches
-    latest:'latest',
-    popular:'popular',
-    now_playing:'now_playing',
-    top_rating:'top_rated'
+
+    let img = document.getElementById('img'); // get the img tag
+    img.setAttribute('src', book_image);
+    document.getElementById('title').textContent = title; //render the title
+
+    document.getElementById('summary').textContent = description;//render the summary
+
+    let a = document.getElementById('amazonURL')
+    a.setAttribute('href', amazon_product_url); //render the amazon link
+
+}
+
+// getNYT();
+
+
+const key1 = '?api_key=' + key.tmdb;
+const baseUrl = 'https://api.themoviedb.org/3/movie/';
+const search = { //object for searches
+    latest: 'latest',
+    popular: 'popular',
+    now_playing: 'now_playing',
+    top_rating: 'top_rated'
 };
-const endUrl='ac&language=en-US&page=1';
+const endUrl = 'ac&language=en-US&page=1';
 
-const imgLink='https://image.tmdb.org/t/p/w500';
+const imgLink = 'https://image.tmdb.org/t/p/w500';
 
-const url=baseUrl+search.popular+key1; //create the URL
+const url = baseUrl + search.popular + key1; //create the URL
 
-async function getMovies(){
-    let ran=Math.floor((Math.random() * 19) + 1) //crate a random number for the index
+async function getMovies() {
+    let ran = Math.floor((Math.random() * 19) + 1) //crate a random number for the index
 
-    const rawData=await fetch(url); // get the raw data
-    const movieData= await rawData.json(); // get the movies data
+    const rawData = await fetch(url); // get the raw data
+    const movieData = await rawData.json(); // get the movies data
 
-    const {poster_path,overview, original_title, }=movieData.results[ran]; //data
+    const { poster_path, overview, original_title, } = movieData.results[ran]; //data
 
-    console.log(imgLink+poster_path);
+    console.log(imgLink + poster_path);
     console.log(original_title);
     console.log(overview);
 
     let img = document.getElementById('img'); // get the img tag
-    img.setAttribute('src', imgLink+poster_path); 
-    document.getElementById('title').textContent=original_title; //render the title
-    
-    document.getElementById('img').appendChild(img); //render the img
-    document.getElementById('summary').textContent=overview; // render the summary
+    img.setAttribute('src', imgLink + poster_path);
+    document.getElementById('title').textContent = original_title; //render the title
+    document.getElementById('summary').textContent = overview; // render the summary
 
 }
 // getMovies()
